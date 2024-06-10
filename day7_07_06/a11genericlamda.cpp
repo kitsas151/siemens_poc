3620d6357e24dca6b8b7596d11b86f988344486e
vasu <vasubg@outlook.com>
2024-06-10 09:28:50 +0530

    updated

--- day7_07_06/a11genericlamda.cpp
+++ day7_07_06/a11genericlamda.cpp
@@ -4,13 +4,25 @@
 
 int main() {
     // Generic lambda to compute the sum of two values
-    auto sum =  [](auto a, auto b){
+    decltype(auto) sum =  [](auto a, auto b){
+
         return a + b;
     };
 
+    auto f1 = []() { static int x=4; int &y=x; return y; };
+
+    std::cout << typeid(f1()).name();
+  
+
+    /*
     std::cout << "Sum of integers: " << sum(1, 6) << "\n";
-    std::cout << "Sum of doubles: " << sum(1.0, 5.6) << "\n";
+    auto ok = sum(10,5.6);
+    std::cout << "Sum of doubles: " <<  "\n";
+
+    */
+
     //std::cout << "Concatenated strings: " << sum("Geeks", "ForGeeks") << "\n";
+
 
     return 0;
 }
